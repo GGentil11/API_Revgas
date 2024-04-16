a5ee624ee4a8729302ee4a980616dee54899b539
Gabriel Felipe Baia Gentil <97031050+GGentil11@users.noreply.github.com>
2024-04-15 22:19:39 -0300

    Update settings.py

--- Banco/settings.py
+++ Banco/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l)hmv(^ys3wzcd08$r$5$35)b()&_!rg*payfn9tm&w4c8famo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
