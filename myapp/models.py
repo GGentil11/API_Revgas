4c0604b50210d2611b3d2018989930e335ee6d83
Gabriel Felipe Baia Gentil <97031050+GGentil11@users.noreply.github.com>
2024-04-15 21:51:57 -0300

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-"""class Instituicao(models.Model):
-    codigo_compensacao = models.IntegerField(db_column='CodigoCompensacao', primary_key=True) 
-    nome_instituicao = models.CharField(db_column='NomeInstituicao', max_length=255)
-
-    def __str__(self):
-        return self.nome_instituicao"""
 class MyappInstituicao(models.Model):
     codigocompensacao = models.IntegerField(db_column='CodigoCompensacao', primary_key=True)  # Field name made lowercase.
     nomeinstituicao = models.CharField(db_column='NomeInstituicao', max_length=255)  # Field name made lowercase.
